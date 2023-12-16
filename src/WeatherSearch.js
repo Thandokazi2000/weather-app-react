b05dda17985025f62523f9ace718090dee836080
Thandokazi2000 <thandonotyalwa@gmail.com>
2023-12-16 22:08:05 +0200

    updated 4 documents

---
+++ src/WeatherSearch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function WeatherSearch() {
+  return (
+    <form>
+      <input type="text" />
+      <input type="text" value="submit" />
+    </form>
+  );
+}
